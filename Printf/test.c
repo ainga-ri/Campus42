d73c7f6b7b9164b453ad9184d999ddfd28d71afc
Deporis <alfred.inga.rios@gmail.com>
2022-07-11 11:24:41 +0200

    PORFA COMMIT

--- Printf/test.c
+++ Printf/test.c
@@ -1,77 +1,13 @@
 #include <stdarg.h>
 #include <stdio.h>
-#include <unistd.h>
 
-void	ft_putstr_fd(char *s, int fd)
-{
-	int	i;
+// Needs to be included libft library, but a Makefile is needed
+#include "libft.h"
 
-	i = 0;
-	while (s[i])
-	{
-		write(fd, &s[i], sizeof(s[i]));
-		i++;
-	}
-}
+// To run it, add the functions needed in this program: gcc test.c ft_putchar_fd.c ft_putnbr_fd.c ft_putstr_fd.c
 
-int     ft_atoi(const char *str)
-{
-        int     i;
-        int     real_num;
-        int     sign;
-
-        sign = 1;
-        i = 0;
-        real_num = 0;
-        while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || str[i] == '\f'
-                || str[i] == '\r' || str[i] == ' ' || str[i] == '+' || str[i] == '-')
-        {
-                if (str[i] == '-')
-                        sign = -sign;
-                if ((str[i] == '-' || str[i] == '+') && (str[i + 1] == '\t'
-                                || str[i + 1] == '\n' || str[i + 1] == '\v'
-                                || str[i + 1] == '\r' || str[i + 1] == ' '
-                                || str[i + 1] == '-' || str[i + 1] == '+'))
-                        return (0);
-                i++;
-        }
-        while (str[i] >= '0' && str[i] <= '9')
-        {
-                real_num = real_num * 10 + (str[i] - 48);
-                i++;
-        }
-        return (sign * real_num);
-}
-
-void	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, sizeof(c));
-}
-
-void    ft_putnbr_fd(int n, int fd)
-{
-        if (n == -2147483648)
-                ft_putstr_fd("-2147483648", fd);
-        else
-        {
-                if (n < 0)
-                {
-                        n = -n;
-                        ft_putchar_fd('-', fd);
-                }
-                if (n < 10)
-                {
-                        ft_putchar_fd(48 + n, fd);
-                }
-                else
-                {
-                        ft_putnbr_fd(n / 10, fd);
-                        ft_putnbr_fd(n % 10, fd);
-                }
-        }
-}
-
-int ft_printf(char const *cadena, ...)
+// use of ssize_t because is used for BUFFER capacity which is our string [-1, MAXINT] because is sexy
+ssize_t ft_printf(char const *cadena, ...)
 {
     int i;
     va_list va_ptr;
